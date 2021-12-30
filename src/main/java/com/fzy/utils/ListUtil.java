173eee25180cd38371a868db94189831d9394c56
yushu <591821761@qq.com>
2021-12-30 13:41:34 +0800

    feat:Stream类(;;)

--- src/main/java/com/fzy/utils/ListUtil.java
+++ src/main/java/com/fzy/utils/ListUtil.java
@@ -1,8 +1,13 @@
 package com.fzy.utils;
 
+import com.alibaba.fastjson.JSONObject;
 import com.sun.istack.internal.NotNull;
 
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.lang.reflect.Field;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.NumberFormat;
 import java.util.Collections;
 import java.util.Comparator;
@@ -158,6 +163,71 @@ public class ListUtil {
         // 设置最小整数位数
         nf.setMinimumIntegerDigits(length);
         return nf.format(numObj);
+    }
+
+
+
+    /**
+     * 读取远程url图片,得到宽高
+     *
+     * @param imgUrl 图片路径
+     * @return [0] 宽  [1]高
+     */
+    public static int[] getImgWH(String imgUrl) {
+        boolean b=false;
+        try {
+            //实例化url
+            URL url = new URL(imgUrl);
+            //载入图片到输入流
+            java.io.BufferedInputStream bis = new BufferedInputStream(url.openStream());
+            //实例化存储字节数组
+            byte[] bytes = new byte[100];
+            //设置写入路径以及图片名称
+            OutputStream bos = new FileOutputStream(new File("pic.jpg"));
+            int len;
+            while ((len = bis.read(bytes)) > 0) {
+                bos.write(bytes, 0, len);
+            }
+            bis.close();
+            bos.flush();
+            bos.close();
+            //关闭输出流
+            b=true;
+        } catch (Exception e) {
+            //如果图片未找到
+            b=false;
+        }
+        int[] a = new int[2];
+        if(b){//图片存在
+            //得到文件
+            java.io.File file = new java.io.File("pic.jpg");
+            BufferedImage bi = null;
+            boolean imgwrong=false;
+            try {
+                //读取图片
+                bi = javax.imageio.ImageIO.read(file);
+                try{
+                    //判断文件图片是否能正常显示,有些图片编码不正确
+                    int i = bi.getType();
+                    imgwrong=true;
+                }catch(Exception ignored){
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            if(imgwrong){
+                a[0] = bi.getWidth(); //获得 宽度
+                a[1] = bi.getHeight(); //获得 高度
+            }else{
+                a=null;
+            }
+            //删除文件
+            file.delete();
+        }else{//图片不存在
+            a=null;
+        }
+        return a;
+
     }
 
 
