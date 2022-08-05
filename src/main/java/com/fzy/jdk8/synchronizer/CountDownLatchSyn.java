e84b735a011cd2987b7dbe7b269c0cefbce79379
赵方（玉书） <yushu@bigrule.com>
2022-08-05 15:30:57 +0800

    feat:(循环栏类)-add

---
+++ src/main/java/com/fzy/jdk8/synchronizer/CountDownLatchSyn.java
@@ -0,0 +1,35 @@
+package com.fzy.jdk8.synchronizer;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+/**
+ *
+ * CountDownLatch 允许一个或多个线程等待直到在其他线程中执行的一组操作完成的同步辅助
+ * <p>
+ *     <li>1.CountDownLatch用给定的计数初始化。 await方法阻塞，直到由于countDown()方法的调用而导致当前计数达到零，之后所有等待线程被释放，并且任何后续的await 调用立即返回。 这是一个一次性的现象 - 计数无法重置。 </li>
+ *     <li></li>
+ * </p>
+ *
+ * @author yushu.zhao
+ * @create 2022/08/05 15:29
+ */
+@Slf4j
+public class CountDownLatchSyn {
+
+
+    /**
+     * ######################################################
+     * #=========================yuShu====================#
+     * ######################################################
+     */
+
+
+
+
+
+
+
+
+
+}
